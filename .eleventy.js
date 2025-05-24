4dc2fb93529bff8982664aebb14c2a627e76aa00
Kay <rn.kay99@icloud.com>
2025-05-24 03:22:29 +0200

    adds author information to blog entry

--- .eleventy.js
+++ .eleventy.js
@@ -7,11 +7,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/manifest.json");
   eleventyConfig.addPassthroughCopy("src/*.ico");
-  eleventyConfig.addPassthroughCopy({ "src/_data/blog_posts.json": "_data/blog_posts.json" });
+  eleventyConfig.addPassthroughCopy({
+    "src/_data/blog_posts.json": "_data/blog_posts.json",
+    "src/_data/creators.json": "_data/creators.json"
+  });
 
   // ⬅️ Custom Filter
   eleventyConfig.addFilter("date", (value, format = "yyyy") => {
     return DateTime.fromJSDate(value).toFormat(format);
+  });
+
+  // Neuer Filter für die Autorenzuordnung
+  eleventyConfig.addFilter("findCreator", function (creators, creatorId) {
+    if (!creators || !creatorId) return {};
+    return creators.find(c => c.creator_id === creatorId) || {};
   });
 
   return {
